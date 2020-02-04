9396d3cc280578f0954a41bc504d517dac17ea6f
nibble4bits <andiyasha16@gmail.com>
2020-02-03 23:56:35 -0600

    Added docstrings for each method

--- solver.py
+++ solver.py
@@ -5,10 +5,12 @@ QUEEN_TOKEN = 'Q'  # the representation of the queen on the board structure
 
 
 def is_queen_safe(board, row, col):
+    '''Checks if a queen can be safely placed in the square specified by (row, col)'''
     return is_queen_safe_row(board, row) and is_queen_safe_col(board, col) and is_queen_safe_diagonals(board, row, col)
 
 
 def is_queen_safe_row(board, row):
+    '''Checks if a queen can be safely placed in the specified row'''
     for y in range(BOARD_SIZE):
         if board[row][y] == QUEEN_TOKEN:
             return False
@@ -16,6 +18,7 @@ def is_queen_safe_row(board, row):
 
 
 def is_queen_safe_col(board, col):
+    '''Checks if a queen can be safely placed in the specified column'''
     for x in range(BOARD_SIZE):
         if board[x][col] == QUEEN_TOKEN:
             return False
@@ -23,6 +26,7 @@ def is_queen_safe_col(board, col):
 
 
 def is_queen_safe_diagonals(board, row, col):
+    '''Checks if a queen can be safely placed in the diagonals crossing through the square specified by (row, col)'''
     for offset in range(1, row + 1):
         if row - offset >= 0 and col + offset < BOARD_SIZE:
             if board[row - offset][col + offset] == QUEEN_TOKEN:
@@ -34,6 +38,11 @@ def is_queen_safe_diagonals(board, row, col):
 
 
 def generate_empty_board():
+    '''Generate and empty chessboard
+
+    Returns:
+        list[list[int]]: A 2D list of all-zeros that represents an empty chessboard
+    '''
     board = []
 
     for i in range(BOARD_SIZE):
@@ -44,6 +53,7 @@ def generate_empty_board():
 
 
 def print_board(board):
+    '''Print the chessboard'''
     for i in range(BOARD_SIZE):
         for j in range(BOARD_SIZE):
             print(board[i][j], end=' ')
@@ -52,6 +62,12 @@ def print_board(board):
 
 
 def solve(board, curr_row=0):
+    '''Recursively solves and prints all solutions to the N-Queens Problem
+    
+    Args:
+        board (list[list[int]]): The board structure on which the problem will be solved
+        curr_row (int): The index of the row where a queen could be placed
+    '''
     if curr_row == BOARD_SIZE:
         print_board(board)
     else:
@@ -62,7 +78,7 @@ def solve(board, curr_row=0):
                 board[curr_row][col] = 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     board = generate_empty_board()
     t1 = time.perf_counter()
     solve(board)
